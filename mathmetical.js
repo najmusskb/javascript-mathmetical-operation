11ac905b65955eb367ac47d11504c7628c05b532
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-05-30 21:56:13 +0600

    first commit

---
+++ mathmetical.js
@@ -0,0 +1,56 @@
+var onionPrize = 43;
+var eggPrize = 42;
+var totalPrice = onionPrize + eggPrize;
+
+// console.log(onionPrize);
+// console.log(eggPrize);
+
+// console.log(onionPrize + eggPrize);
+
+console.log(totalPrice);
+
+
+
+
+var onionPrize = 43;
+var eggPrize = 42;
+
+var totalPrice = onionPrize - eggPrize;
+
+// console.log(onionPrize);
+// console.log(eggPrize);
+
+// console.log(onionPrize + eggPrize);
+
+console.log(totalPrice);
+
+
+
+
+
+var onionPrize = 43;
+var eggPrize = 42;
+
+var totalPrice = onionPrize * eggPrize;
+
+// console.log(onionPrize);
+// console.log(eggPrize);
+
+// console.log(onionPrize + eggPrize);
+console.log(totalPrice);
+
+
+
+
+var MoneyAbleable = 100;
+var eggPrize = 5;
+
+var totalPrice = MoneyAbleable / eggPrize;
+
+// console.log(onionPrize);
+// console.log(eggPrize);
+
+// console.log(onionPrize + eggPrize);
+
+console.log(totalPrice);
+
